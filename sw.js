eaf094e1d2c4aa1f681fbc2e50b4923e3a56ad28
ezzatalnajm-dotcom <ezzat.alnajm@gmail.com>
2026-08-09 11:12:52 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+// Minimal service worker to satisfy PWA installability criteria on Chrome/Android
+const CACHE_NAME = 'shajarat-alaila-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
